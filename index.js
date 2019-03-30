b4cd04383d034e1b5af2e1cb4a15788371455e31
Francisco Vilches <f_vilches17@outlook.com>
2019-03-30 20:04:06 +1300

    Refactor

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const doItButton = document.querySelector("#btn-doit");
-const counterButton = document.querySelector("#btn-count");
-const doItDisplayArea = document.querySelector("#display-area-doit");
-const counterDisplayArea = document.querySelector("#display-area-count");
-const importObject = { imports: { imported_func: arg => console.log(arg) } };
-
-fetch('index.wasm').then(response =>
-    response.arrayBuffer()
-).then(bytes =>
-    WebAssembly.instantiate(bytes, importObject)
-).then(results => {
-    const wasmExports = results.instance.exports;
-
-    doItButton.onclick = ()=> {
-        doItDisplayArea.innerText = wasmExports.MyCLanguageDoItFunction();
+(async function() {
+    const doItButton = document.querySelector("#btn-doit");
+    const counterButton = document.querySelector("#btn-count");
+    const doItDisplayArea = document.querySelector("#display-area-doit");
+    const counterDisplayArea = document.querySelector("#display-area-count");
+    const importObject = { imports: { imported_func: arg => console.log(arg) } };
+    
+    try {
+        const response = await fetch("index.wasm");
+        const bytes = await response.arrayBuffer();
+        const results = await WebAssembly.instantiate(bytes, importObject);
+        const wasmExports = results.instance.exports;
+        
+        doItButton.onclick = () => {
+            doItDisplayArea.innerText = wasmExports.MyCLanguageDoItFunction();
+        }
+    
+        counterButton.onclick = () => {
+            counterDisplayArea.innerText = wasmExports.MyCLanguageCounterFunction(1);
+        }    
+    } catch (error) {
+        console.error(error);
     }
-
-    counterButton.onclick = ()=> {
-        counterDisplayArea.innerText = wasmExports.MyCLanguageCounterFunction(1);
-    }
-
-}).catch(console.error);
+})();
